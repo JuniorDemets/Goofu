65f3697e09268def8e9c7281bfc99542b23dd6f7
JuniorDemets <Demetsjunior25@gmail.com>
2022-01-29 22:52:36 +0100

    Page d'inscription et connexion finie + amélioration du code php pour insérer dans la base de donnée

--- php/recup_form.php
+++ php/recup_form.php
@@ -5,10 +5,10 @@
     $pass = "";
     
     $Nom = $_POST["Nom"];
-    $Prénom = $_POST["Prénom"];
+    $Prénom = $_POST["Prenom"];
     $Genre = $_POST["Genre"];
     $E_mail = $_POST["E_mail"];
-    $Mot_de_passe = $_POST["Mot de passe"];
+    $Mot_de_passe = md5($_POST["Mot_de_passe"]);
 
     try{
         //On se connecte à la BDD
@@ -16,14 +16,13 @@
         $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
         //On insère les données reçues
-        $sth = $dbco->prepare("
-            INSERT INTO form(Nom, Prénom, Genre, E_mail, Mot_de_passe)
-            VALUES(:Nom, :Prénom, :Genre, :E_mail, :Mot de passe)");
+        $sth = $dbco -> prepare("INSERT INTO goofu(Nom, Prenom, Genre, E_mail, Mot_de_passe)
+        VALUES ('$Nom', '$Prénom', '$Genre', '$E_mail', '$Mot_de_passe')");
         $sth->bindParam(':Nom',$Nom);
-        $sth->bindParam(':Prénom',$Prénom);
+        $sth->bindParam(':Prenom',$Prénom);
         $sth->bindParam(':Genre',$Genre);
         $sth->bindParam(':E_mail',$E_mail);
-        $sth->bindParam(':Mot de passe',$Mot_de_passe);
+        $sth->bindParam(':Mot_de_passe',$Mot_de_passe);
         $sth->execute();
         
         //On renvoie l'utilisateur vers la page de remerciement
