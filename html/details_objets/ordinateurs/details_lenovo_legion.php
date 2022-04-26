d7af0cb60c8c6ce40d53856fc27a365ec5594ec8
SierraL01 <79834072+SierraL01@users.noreply.github.com>
2022-04-26 19:11:11 +0200

    debut pc php

---
+++ html/details_objets/ordinateurs/details_lenovo_legion.php
@@ -0,0 +1,110 @@
+<?php>
+//  Cookies pour la connexion sur tout le site
+    $E_mail = $_POST['E_mail'];
+    setcookie("Cookie", $E_mail, time()+3600000000000000000000000000000000000000000000000000000000000000000000000000000000000000);
+    if (isset($_COOKIE["Cookie"])){ 
+        echo 'Le cookie existe ' . $_COOKIE["Cookie"] . '!<br />';
+    }else
+        echo 'Le cookie n\'existe pas <br />';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="../../../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../../../css/details.css">
+        <script src="ajouter_panier.js"></script> 
+        <title>Legion</title>
+    </head>
+    <body>
+        <!-- haut de page + logo + recherche  + panier + connexion-->
+        <div class="pageup">
+            <a href="../../index.php"><img class="logo" src="https://via.placeholder.com/140x100" alt="logo"></a>
+        </div>
+        <div class="search_total">
+            <input class="barre_cherche" placeholder="Recherche..." >
+            <input class="bouton_rechercher"  type="submit" value="Rechercher"> 
+                <form action="../panier.php">
+                    <input class="panier" type="submit" value="Panier">
+                </form>
+                <form action="../../connexion.php" target="_blank">
+                    <input class="connexion" type="submit" value="Connexion">
+                </form>
+                <form action="../../compte.php" target="_blank">
+                    <input class="compte" type="submit" value="Compte">
+                </form>
+        </div>   
+         <!--choix des catégories (nav bar)-->
+        <div class="category_total">
+            <form action="../../categories/ordinateurs.php">
+                <input class="ordinateur" type="submit" value="Ordinateurs">
+            </form>
+            <form action="../../categories/telephones.php">
+                <input class="telephone" type="submit" value="Téléphones">
+            </form>
+            <form action="../../categories/vr.php">
+                <input class="realite_virtuelle" type="submit" value="Réalite Virtuelle">
+            </form>
+            <form action="../../categories/console.php">
+                <input class="console" type="submit" value="Consoles">
+            </form>
+            <form action="../../categories/giftcard.php">
+                <input class="cartes_cadeau" type="submit" value="Cartes Cadeau">
+            </form>
+        </div>
+        <!--Boite qui prends tout-->
+        <div class="all_items">
+            <div id="img_wishlist">
+                <img src="../../../images/category/pc_legion.avif" class="legion_image" alt="pc legion">
+            </div>
+            <form action="....">
+                <input class="ajouter_liste_souhaits" type="submit" value="Ajouter à la Liste des Souhaits">
+            </form>
+            <div class="titre_description">
+                <h2 class="details">Détails du produit :</h2>
+                <p class="description_complete">
+                    Jouez aux derniers jeux vidéos à des paramètres élevés avec le Lenovo Legion T5 26AMR5 90RC00LTMH. 
+                                          <br>
+                                          La carte graphique NVIDIA GeForce RTX 3060 est suffisamment puissante pour ça. 
+                                          <br>
+                                          La carte graphique convient aussi à la VR, branchez donc un casque VR et jouez à votre jeu vidéo favori en réalité virtuelle. 
+                                          <br>
+                                          Jouez pendant que vous discutez avec vos amis sur Discord grâce aux 16 Go de mémoire vive.
+                                          <br>
+                                          Installez vos jeux préférés sur le SSD de 512 Go pour qu'ils démarrent rapidement.                      
+                </p>
+                </div>
+            </div>
+            <div class="prix_panier">
+                <p class="prix">1299€</p>
+                <form action="add_cart">
+                    <input class="ajouter_panier" type="submit" value="Ajouter au panier">
+                </form>
+                <select name="Nombre d'ajouts" id="nbre_panier">
+                    <option value="">--Quantité--</option>
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="0">6</option>
+                    <option value="1">7</option>
+                    <option value="2">8</option>
+                    <option value="3">9</option>
+                    <option value="4">10</option>
+                </select>
+            </div>
+            <!--Commentaires sur l'article-->
+            <form action="commentaire.php">
+                <textarea class="ajouter_commentaire" name="commentaires"
+                rows="5" cols="33" placeholder="Ajoutez un commentaire..."></textarea>
+            <!--Bouton-->
+                <input class="bt_send_com" type="submit" value="Envoyer">
+            </form>   
+        </div>
+        <div class="signature">Standaert-De Clercq Simon  et Demets Junior </div>         
+    </body>
+</html>
