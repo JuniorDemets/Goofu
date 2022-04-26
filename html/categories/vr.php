5f40eef02a3d0ad40b2569d559fe2c2733194e4e
SierraL01 <79834072+SierraL01@users.noreply.github.com>
2022-04-26 19:07:17 +0200

    php sur page détails téléphones

---
+++ html/categories/vr.php
@@ -0,0 +1,127 @@
+<?php>
+//  Cookies pour la connexion sur tout le site
+    $E_mail = $_POST['E_mail'];
+    setcookie("Cookie", $E_mail, time()+3600000000000000000000000000000000000000000000000000000000000000000000000000000000000000);
+    if (isset($_COOKIE["Cookie"])){ 
+        echo 'Le cookie existe ' . $_COOKIE["Cookie"] . '!<br />';
+    }else
+        echo 'Le cookie n\'existe pas <br />';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="../../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../../css/category.css">
+        <script src="ajouter_panier.js"></script> 
+        <title>Réalité Virtuelle</title>
+    </head>
+    <body>
+        <!-- haut de page + logo + recherche  + panier + connexion-->
+        <div class="pageup">
+            <a href="../index.php"><img class="logo" src="https://via.placeholder.com/140x100" alt="logo"></a>
+            <div class="search_total">
+                <input class="barre_cherche" placeholder="Recherche..." >
+                <input class="bouton_rechercher"  type="submit" value="Rechercher">
+            </div> 
+            <form action="../panier.php">
+                <input class="panier" type="submit" value="Panier">
+            </form>
+            <form action="../connexion.php" target="_blank">
+                <input class="connexion" type="submit" value="Connexion">
+            </form>
+            <form action="../compte/compte.php" target="_blank">
+                <input class="compte" type="submit" value="Compte">
+            </form>
+        </div>
+        <!--choix des catégories (nav bar)-->
+        <div class="category_total">
+            <form action="ordinateurs.php">
+                <input class="ordinateur" type="submit" value="Ordinateurs">
+            </form>
+            <form action="telephones.php">
+                <input class="telephone" type="submit" value="Téléphones">
+            </form>
+            <form action="vr.php">
+                <input class="realite_virtuelle" type="submit" value="Réalite Virtuelle">
+            </form>
+            <form action="console.php">
+                <input class="console" type="submit" value="Consoles">
+            </form>
+            <form action="giftcard.php">
+                <input class="cartes_cadeau" type="submit" value="Cartes Cadeau">
+            </form>
+        </div>
+        <!-- boite pc msi (présentation) +  bouton pour voir le pc msi -->
+        <div class="choix_quest">
+            <img class="photo_quest" src="../../images/category/oculus-quest.png" alt="Image1">
+            <h2 id="affichage_prix_1">449€</h2>
+            <p id="description_quest_1_head">Oculus Quest 2 :</p>
+            <p id="description_quest_1">
+                Oculus Quest 2 est notre système de VR tout-en-un le plus abouti.
+                Chaque détail a été conçu pour retranscrire au mieux vos mouvements dans l’univers virtuel. 
+                Vous profitez ainsi de jeux et d’expériences à couper le souffle avec une liberté inégalée. </p>
+            <form action="../details_objets/vr/details_oculus_quest.php" >
+                <input class="quest_1" type="submit" value="Détails">
+            </form>
+            <form action="ajouter_panier.php" >
+                <input class="bouton__panier_1" type="submit" value="+">
+            </form>
+        <!-- Fin boite un -->
+        <!-- boite pc asuz (présentation) +  bouton pour voir le pc asuz -->
+        <!-- <div class="choix_htc">
+            <img class="photo_htc" src="../images/category/150.png" alt="Image2">
+            <h2 id="affichage_prix_1">$00.00</h2>
+            <p id="description_htc_1_head">PC Gamer de marque htc :</p>
+            <p id="description_htc_1">Ce pc de marque htc exellent pour gaming next-gen. </p>
+            <form action="htc.php" >
+                <input class="htc_1" type="submit" value="Détails">
+            </form>
+            <form action="ajouter_panier2.php" >
+                <input class="bouton__panier_1" type="submit" value="+">
+            </form> -->
+        <!-- Fin boite un -->
+        <!-- boite pc asuz (présentation) +  bouton pour voir le pc asuz -->
+        <!-- <div class="choix_oneplus">
+            <img class="photo_oneplus" src="../images/category/" alt="Image2">
+            <h2 id="affichage_prix_1">$00.00</h2>
+            <p id="description_oneplus_1_head">Téléphone gaming de marque OnePlus :</p>
+            <p id="description_oneplus_1">Téléphone gaming de marque OnePlus exellent pour gaming next-gen. </p>
+            <form action="oneplus.php" >
+                <input class="oneplus_1" type="submit" value="Détails">
+            </form>
+            <form action="ajouter_panier2.php" >
+                <input class="bouton__panier_1" type="submit" value="+">
+            </form> -->
+        <!-- Fin boite un -->
+        <!-- boite pc asuz (présentation) +  bouton pour voir le pc asuz -->
+        <!-- <div class="choix_asuz">
+            <img class="photo_asuz" src="../images/category/150.png" alt="Image2">
+            <h2 id="affichage_prix_1">$00.00</h2>
+            <p id="description_asuz_1_head">PC Gamer de marque Asuz :</p>
+            <p id="description_asuz_1">Ce pc de marque Asuz exellent pour gaming next-gen. </p>
+            <form action="asuz.php" >
+                <input class="asuz_1" type="submit" value="Détails">
+            </form>
+            <form action="ajouter_panier2.php" >
+                <input class="bouton__panier_1" type="submit" value="+">
+            </form> -->
+        <!-- Fin boite un -->
+        <!-- boite pc asuz (présentation) +  bouton pour voir le pc asuz -->
+        <!-- <div class="choix_asuz">
+            <img class="photo_asuz" src="../images/category/150.png" alt="Image2">
+            <h2 id="affichage_prix_1">$00.00</h2>
+            <p id="description_asuz_1_head">PC Gamer de marque Asuz :</p>
+            <p id="description_asuz_1">Ce pc de marque Asuz exellent pour gaming next-gen. </p>
+            <form action="asuz.php" >
+                <input class="asuz_1" type="submit" value="Détails">
+            </form>
+            <form action="ajouter_panier2.php" >
+                <input class="bouton__panier_1" type="submit" value="+">
+            </form> -->
+        <!-- Fin boite un -->
+        <div class="signature">Standaert-De Clercq Simon  et Demets Junior </div>
+    </body>
+</html>
---
+++ html/details_objets/Telephones/details_asuz_rog_phone.php
@@ -0,0 +1,108 @@
+<?php>
+//  Cookies pour la connexion sur tout le site
+    $E_mail = $_POST['E_mail'];
+    setcookie("Cookie", $E_mail, time()+3600000000000000000000000000000000000000000000000000000000000000000000000000000000000000);
+    if (isset($_COOKIE["Cookie"])){ 
+        echo 'Le cookie existe ' . $_COOKIE["Cookie"] . '!<br />';
+    }else
+        echo 'Le cookie n\'existe pas <br />';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="../../../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../../../css/details.css">
+        <script src="ajouter_panier.js"></script> 
+        <title>Asuz phone</title>
+    </head>
+    <body>
+        <!-- haut de page + logo + recherche  + panier + connexion-->
+        <div class="pageup">
+            <a href="../../index.php"><img class="logo" src="https://via.placeholder.com/140x100" alt="logo"></a>
+        </div>
+        <div class="search_total">
+            <input class="barre_cherche" placeholder="Recherche..." >
+            <input class="bouton_rechercher"  type="submit" value="Rechercher"> 
+                <form action="../../panier.php">
+                    <input class="panier" type="submit" value="Panier">
+                </form>
+                <form action="../../connexion.php" target="_blank">
+                    <input class="connexion" type="submit" value="Connexion">
+                </form>
+                <form action="../../compte.php" target="_blank">
+                    <input class="compte" type="submit" value="Compte">
+                </form>
+        </div>   
+         <!--choix des catégories (nav bar)-->
+        <div class="category_total">
+            <form action="../../categories/ordinateurs.php">
+                <input class="ordinateur" type="submit" value="Ordinateurs">
+            </form>
+            <form action="../../categories/telephones.php">
+                <input class="telephone" type="submit" value="Téléphones">
+            </form>
+            <form action="../../categories/vr.php">
+                <input class="realite_virtuelle" type="submit" value="Réalite Virtuelle">
+            </form>
+            <form action="../../categories/console.php">
+                <input class="console" type="submit" value="Consoles">
+            </form>
+            <form action="../../categories/giftcard.php">
+                <input class="cartes_cadeau" type="submit" value="Cartes Cadeau">
+            </form>
+        </div>
+        <!--Boite qui prends tout-->
+        <div class="all_items">
+            <div id="img_wishlist">
+                <img src="../../../images/category/tel_asuz.jpg" class="msi_image" alt="pc msi">
+            </div>
+            <form action="....">
+                <input class="ajouter_liste_souhaits" type="submit" value="Ajouter à la Liste des Souhaits">
+            </form>
+            <div class="titre_description">
+                <h2 class="details">Détails du produit :</h2>
+                <p class="description_complete">
+                    Jouez au maximum des possibilités avec le ROG Phone 5, le smartphone gaming qui ne fait aucun prisonnier. 
+                    <br>
+                    Propulsé pour gagner par la dernière plateforme mobile 5G Qualcomm® Snapdragon™ 888, cette merveille futuriste est équipée d’un écran incroyablement réactif de 144 Hz et d’1ms, 
+                    <br>
+                    d’un monstrueux système de batterie de 6000 mAh1, des contrôles de jeux largement améliorés AirTrigger 5 et de notre système audio iconique GameFX. 
+                    <br>
+                    Le ROG Phone 5 amènera votre niveau de jeu dans une nouvelle dimension, si vous l’osez !
+                </p>
+                </div>
+            </div>
+            <div class="prix_panier">
+                <p class="prix">999€</p>
+                <form action="add_cart">
+                    <input class="ajouter_panier" type="submit" value="Ajouter au panier">
+                </form>
+                <select name="Nombre d'ajouts" id="nbre_panier">
+                    <option value="">--Quantité--</option>
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="0">6</option>
+                    <option value="1">7</option>
+                    <option value="2">8</option>
+                    <option value="3">9</option>
+                    <option value="4">10</option>
+                </select>
+            </div>
+            <!--Commentaires sur l'article-->
+            <form action="commentaire.php">
+                <textarea class="ajouter_commentaire" name="commentaires"
+                rows="5" cols="33" placeholder="Ajoutez un commentaire..."></textarea>
+            <!--Bouton-->
+                <input class="bt_send_com" type="submit" value="Envoyer">
+            </form>   
+        </div>
+        <div class="signature">Standaert-De Clercq Simon  et Demets Junior </div>         
+    </body>
+</html>
